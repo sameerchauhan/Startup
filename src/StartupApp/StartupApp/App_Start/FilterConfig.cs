eae84cc5f192e094e15937e15c454b10210da64a
Sameer Chauhan <sameerzw@hotmail.com>
2014-06-06 15:31:19 +0100

    Basic MVC application with test project

---
+++ src/StartupApp/StartupApp/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace StartupApp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
