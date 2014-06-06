9a368b57a9fcf4ec18ea986a3aa4f97da0385fb1
Sameer Chauhan <sameerzw@hotmail.com>
2014-06-06 22:16:31 +0100

    Added OWIN security

---
+++ src/StartupApp/StartupApp/App_Start/IdentityConfig.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+
+namespace StartupApp
+{
+    // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
+
+    public class EmailService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            // Plug in your email service here to send an email.
+            return Task.FromResult(0);
+        }
+    }
+
+    public class SmsService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            // Plug in your sms service here to send a text message.
+            return Task.FromResult(0);
+        }
+    }
+}
