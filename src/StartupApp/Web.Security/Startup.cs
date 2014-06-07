3201c5ba08d1b2015c75b1c90feef3491980257e
Sameer Chauhan <sameerzw@hotmail.com>
2014-06-07 06:42:53 +0100

    Move security aspects of application to separate project so

---
+++ src/StartupApp/Web.Security/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Web.Security;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace Web.Security
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
