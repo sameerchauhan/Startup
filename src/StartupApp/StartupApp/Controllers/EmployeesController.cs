52b57340f0ea33be00321ad8b7c3a4f97a71a66b
Sameer Chauhan <sameerzw@hotmail.com>
2014-06-10 16:50:19 +0100

    Updated Link and Added New Controller

---
+++ src/StartupApp/StartupApp/Controllers/EmployeesController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Web.UI.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        public ActionResult Index()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
