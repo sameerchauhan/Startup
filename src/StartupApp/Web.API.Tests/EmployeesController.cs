c08c8f287da164930b5ef0bab546f230c452de87
Sameer Chauhan <sameerzw@hotmail.com>
2014-06-10 21:50:47 +0100

    Added Employee Servicee logic

---
+++ src/StartupApp/Web.API.Tests/EmployeesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Repository;
+
+namespace Web.API.Tests
+{
+    public class EmployeesController
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public EmployeesController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void GetEmployees(int pageSize)
+        {
+            _unitOfWork.EmployeeRepository.GetEmployees(pageSize);
+        }
+    }
+}
---
+++ src/StartupApp/Web.Service/EmployeeService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Web.Service
+{
+    public class EmployeeService : IEmployeeService
+    {
+        public IEnumerable<EmployeeListDto> GetEmployees(int pageSize)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:55665/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                
+                var response = client.GetAsync("api/employee?pageSize="+pageSize).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var employees = response.Content.ReadAsAsync<IEnumerable<EmployeeListDto>>().Result;
+                    return employees;
+                }
+
+            }
+
+            return null;
+        }
+    }
+}
