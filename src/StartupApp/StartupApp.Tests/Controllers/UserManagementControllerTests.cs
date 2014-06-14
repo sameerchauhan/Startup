8a8ac28ce6ffc2d95df7abfeee584c1b13e79d04
Sameer Chauhan <sameerzw@hotmail.com>
2014-06-15 00:00:20 +0100

    Added first Unit Test for UserManagementController

---
+++ src/StartupApp/StartupApp.Tests/Controllers/UserManagementControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace StartupApp.Tests.Controllers
+{
+    [TestClass]
+    public class UserManagementControllerTests
+    {
+        [TestMethod]
+        public void ShouldCallServiceToShowFirst20Users()
+        {
+            var list = new List<UserDto>();
+            var userManagementService = new Mock<IUserManagementService>();
+            userManagementService.Setup(ums => ums.GetUsers(20)).Returns(list);
+            var controller = new UserManagementController(userManagementService.Object);
+
+            userManagementService.Verify(ums => ums.GetUsers(20), Times.Once);
+
+        }
+    }
+
+    public class UserManagementController
+    {
+        public UserManagementController(IUserManagementService userManagementService)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IUserManagementService
+    {
+        IEnumerable<UserDto> GetUsers(int pageSize);
+    }
+
+    public class UserDto
+    {
+    }
+}
